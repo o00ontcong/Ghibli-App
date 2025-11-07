3e6637bca06266f8be51b3b819fc681ab75545f6
ThanhCong <o00ontcong@gmail.com>
2025-11-07 12:00:50 +0700

    init project

---
+++ GhibliSwiftUIApp/GhibliSwiftUIApp/GhibliSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppApp.swift
+//  GhibliSwiftUIApp
+//
+//  Created by ThanhCong on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliSwiftUIApp/GhibliSwiftUIAppTests/GhibliSwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppTests.swift
+//  GhibliSwiftUIAppTests
+//
+//  Created by ThanhCong on 7/11/25.
+//
+
+import Testing
+@testable import GhibliSwiftUIApp
+
+struct GhibliSwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
