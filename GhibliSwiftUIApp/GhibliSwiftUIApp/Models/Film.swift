9cd97fcb485672c351dac424d849f0107b4f6798
ThanhCong <o00ontcong@gmail.com>
2025-11-07 12:29:41 +0700

    step 1

---
+++ GhibliSwiftUIApp/GhibliSwiftUIApp/Models/Film.swift
@@ -0,0 +1,50 @@
+//
+//  Film.swift
+//  GhibliSwiftUIApp
+//
+//  Created by ThanhCong on 7/11/25.
+//
+import Foundation
+
+struct Film:Codable,Identifiable {
+    let id: String
+    let title: String
+    let description: String
+    let director: String
+    let producer: String
+    
+    let releaseYear: String
+    let score: String
+    let duration: String
+    let image: String
+    let bannerImage: String
+    
+    let people: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, image, description, director, producer, people
+        
+        case bannerImage = "movie_banner"
+        
+        case releaseYear = "release_date"
+        case duration = "running_time"
+        case score = "rt_score"
+    }
+    
+
+}
+
+import Playgrounds
+
+#Playground {
+    let url = URL(string: "https://ghibliapi.vercel.app/films")!
+    
+    do {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        try JSONDecoder().decode([Film].self, from: data)
+    } catch {
+        print(error)
+    }
+
+}
